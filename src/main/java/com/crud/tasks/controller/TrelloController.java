27539c76d61763c5f093e15ce89f1da64ac8200b
jakubG78 <jakub.gora@wp.pl>
2020-05-20 18:38:25 +0200

    heroku update

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/trello")
 public class TrelloController {
@@ -27,3 +27,5 @@ public class TrelloController {
         return trelloClient.createNewCard(trelloCardDto);
     }
 }
+
+
