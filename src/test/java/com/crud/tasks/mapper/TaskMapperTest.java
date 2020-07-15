c1bb5ae865ac5b926747b7114c4e922f47a8d439
jakubG78 <jakub.gora@wp.pl>
2020-07-15 20:19:26 +0200

    Commit for m29.1 v3

--- src/test/java/com/crud/tasks/mapper/TaskMapperTest.java
+++ src/test/java/com/crud/tasks/mapper/TaskMapperTest.java
@@ -2,14 +2,15 @@ package com.crud.tasks.mapper;
 
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
-import com.crud.tasks.domain.TrelloCard;
-import com.crud.tasks.domain.TrelloCardDto;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -45,4 +46,21 @@ public class TaskMapperTest {
         System.out.println(mappedTaskDto);
     }
 
+    @Test
+    public void testToTaskDtoList() {
+        //Given
+        Task task1 = new Task(15L, "Test task #1", "Test mapping TaskDto to Task domain object");
+        Task task2 = new Task(11L, "Test task #2", "Test mapping Task to TaskDto object");
+        List<Task> taskList = new ArrayList<>();
+        taskList.add(task1);
+        taskList.add(task2);
+
+        //When
+        List<TaskDto> mappedTaskDtoList = taskMapper.mapToTaskDtoList(taskList);
+
+        //Then
+        Assert.assertEquals(2, mappedTaskDtoList.size());
+        Assert.assertEquals(11L, taskList.get(1).getId(), 0);
+        Assert.assertEquals("Test task #1", taskList.get(0).getTitle());
+    }
 }
