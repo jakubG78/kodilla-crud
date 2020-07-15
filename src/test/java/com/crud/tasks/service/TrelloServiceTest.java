d8162fb621a0bbd5c5bae4747d1a6ef52ed80722
jakubG78 <jakub.gora@wp.pl>
2020-07-15 22:20:04 +0200

    Commit for m29.2

---
+++ src/test/java/com/crud/tasks/service/TrelloServiceTest.java
@@ -0,0 +1,82 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloListDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TrelloServiceTest {
+
+    @InjectMocks
+    private TrelloService trelloService;
+
+    @Mock
+    private AdminConfig adminConfig;
+
+    @Mock
+    private TrelloClient trelloClient;
+
+    @Mock
+    private SimpleEmailService emailService;
+
+    @Test
+    public void shouldFetchEmptyList() {
+        //Given
+        when(trelloClient.getTrelloBoards()).thenReturn(new ArrayList<>());
+
+        //When
+        List<TrelloBoardDto> trelloBoardDtos = trelloService.fetchTrelloBoards();
+
+        //Then
+        assertNotNull(trelloBoardDtos);
+        assertEquals(0, trelloBoardDtos.size());
+    }
+
+    @Test
+    public void shouldFetchTrelloBoards() {
+        //Given
+        List<TrelloListDto> trelloLists = new ArrayList<>();
+        trelloLists.add(new TrelloListDto("2", "test_list", false));
+
+        List<TrelloBoardDto> trelloBoards = new ArrayList<>();
+        trelloBoards.add(new TrelloBoardDto("1", "test_board", trelloLists));
+
+        when(trelloClient.getTrelloBoards()).thenReturn(trelloBoards);
+
+        //When
+        List<TrelloBoardDto> trelloBoardDtos = trelloService.fetchTrelloBoards();
+
+        //Then
+        assertNotNull(trelloBoardDtos);
+        assertEquals(1, trelloBoardDtos.size());
+
+        trelloBoardDtos.forEach(trelloBoardDto -> {
+            assertEquals("1", trelloBoardDto.getId());
+            assertEquals("test_board", trelloBoardDto.getName());
+
+            trelloBoardDto.getLists().forEach(trelloListDto -> {
+                assertEquals("2", trelloListDto.getId());
+                assertEquals("test_list", trelloListDto.getName());
+                assertEquals(false, trelloListDto.isClosed());
+            });
+        });
+    }
+
+//    public List<TrelloBoardDto> fetchTrelloBoards() {
+//        return trelloClient.getTrelloBoards();
+//    }
+
+}
